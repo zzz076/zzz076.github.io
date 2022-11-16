fa6d4c08dea70f0295860c5e03ec5fcb2e28fa6c
tim <zzz076@yahoo.com.tw>
2022-11-16 14:34:35 +0800

    first-time

---
+++ Portfolio/resume/src/index.js
@@ -0,0 +1,12 @@
+const resumeButton = document.getElementById('resumeBtn');
+const portfolioButton = document.getElementById('portfolioBtn');
+const app = document.querySelector('.app');
+
+resumeButton.addEventListener('click', function (event) {
+   window.scrollTo(0,2150);
+  });
+
+  portfolioButton.addEventListener('click', function (event) {
+    window.scrollTo(0,750);
+   });
+  
